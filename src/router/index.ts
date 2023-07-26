21c5390019f8d1bbe46a38db2b705139ea30c0a7
xiaoxiaoxiaohao_company <xiaoxiaoxiaohao@tom.com>
2023-07-26 15:13:22 +0800

    格式化代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,121 +25,121 @@ const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: '',
-      component: Login,
+        path: '/',
+        name: '',
+        component: Login,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: Login,
+        path: '/login',
+        name: 'login',
+        component: Login,
     },
     {
-      path: '/home',
-      name: 'home',
-      component: Home,
-      redirect: {name: "warehousing"}, 
-      meta: {
-        hideTabbar: true,
-        tabbarNum: 0
-      },
+		path: '/home',
+		name: 'home',
+		component: Home,
+		redirect: {name: "warehousing"}, 
+		meta: {
+			hideTabbar: true,
+			tabbarNum: 0
+		},
       children: [
         {
-          path: 'warehousing',
-          name: 'warehousing',
-          component: Warehousing,
+			path: 'warehousing',
+			name: 'warehousing',
+			component: Warehousing,
+		},
+        {
+			path: 'tallying',
+			name: 'tallying',
+			component: Tallying,
         },
         {
-          path: 'tallying',
-          name: 'tallying',
-          component: Tallying,
+			path: 'outbound',
+			name: 'outbound',
+			component: Outbound,
         },
         {
-          path: 'outbound',
-          name: 'outbound',
-          component: Outbound,
+			path: 'loading',
+			name: 'loading',
+			component: Loading,
         },
         {
-          path: 'loading',
-          name: 'loading',
-          component: Loading,
+			path: 'confirmation',
+			name: 'confirmation',
+			component: Confirmation,
         },
         {
-          path: 'confirmation',
-          name: 'confirmation',
-          component: Confirmation,
-        },
-        {
-          path: 'abnormal',
-          name: 'abnormal',
-          component: Abnormal,
+			path: 'abnormal',
+			name: 'abnormal',
+			component: Abnormal,
         },
       ]
     },
     {
-      path: '/filings',
-      name: 'filings',
-      component: Filings,
-      meta: {
-        hideTabbar: true,
-        tabbarNum: 1
-      }
+		path: '/filings',
+		name: 'filings',
+		component: Filings,
+		meta: {
+			hideTabbar: true,
+			tabbarNum: 1
+		}
     },
     {
-      path: '/addcar',
-      name: 'addcar',
-      component: AddCar,
+		path: '/addcar',
+		name: 'addcar',
+		component: AddCar,
     },
     {
-      path: '/updatecar',
-      name: 'updatecar',
-      component: UpdateCar,
+		path: '/updatecar',
+		name: 'updatecar',
+		component: UpdateCar,
     },
     {
-      path: '/user',
-      name: 'user',
-      component: UserView,
-      meta: {
-        hideTabbar: true,
-        tabbarNum: 2
-      }
+		path: '/user',
+		name: 'user',
+		component: UserView,
+		meta: {
+			hideTabbar: true,
+			tabbarNum: 2
+		}
     },
     {
-      path: '/changepsw',
-      name: 'changepsw',
-      component: ChangePsw
+		path: '/changepsw',
+		name: 'changepsw',
+		component: ChangePsw
     },
     {
-      path: '/detailinfo',
-      name: 'detailinfo',
-      component: DetailInfo
+		path: '/detailinfo',
+		name: 'detailinfo',
+		component: DetailInfo
     },
     {
-      path: '/tablechart',
-      name: 'tablechart',
-      component: TableChart,
-      redirect: {name: "tallyingtable"}, 
-      children: [
-        {
-          path: 'tallyingtable',
-          name: 'tallyingtable',
-          component: TallyingTable
-        }
-      ]
+		path: '/tablechart',
+		name: 'tablechart',
+		component: TableChart,
+		redirect: {name: "tallyingtable"}, 
+		children: [
+			{
+				path: 'tallyingtable',
+				name: 'tallyingtable',
+				component: TallyingTable
+			}
+		]
     }
   ]
 })
 
 //路由守卫
 router.beforeEach((to, from, next) => {
-  let homeStore = useHomeStore()
-  if(to.path == '/login' || to.path  == '/tablechart/tallyingtable')  {
-    next()
-  }else if(homeStore.user.PERSON_CODE){   //判断是否已经登录
-    next()
-  }else {
-    router.push({name: 'login'})
-  }
+	let homeStore = useHomeStore()
+	if(to.path == '/login' || to.path  == '/tablechart/tallyingtable')  {
+		next()
+	}else if(homeStore.user.PERSON_CODE){   //判断是否已经登录
+		next()
+	}else {
+		router.push({name: 'login'})
+	}
 })
 
 export default router
